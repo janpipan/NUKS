93f2f0b985fc0fe707779be96e9aa09034ac813f
Jan Pipan <janpipan@gmail.com>
2023-04-03 23:39:39 +0200

    added onSubmit redirect

--- project/client/src/pages/PollCreate.js
+++ project/client/src/pages/PollCreate.js
@@ -1,28 +1,31 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const PollCreate = () => {
-    const [title, setTitle] = useState('');
-    const [author, setAuthor] = useState('');
-    const [multipleAnswers, setMultipleAnswers] = useState(false);
-    const [addAnswers, setAddAnswers] = useState(false);
+    const [data, setData] = useState({
+        title: '',
+        author: '',
+        multipleAnswers: false,
+        addAnswers: false,
+    });
+    const navigate = useNavigate();
 
     const onSubmit = async (event) => {
         event.preventDefault();
 
-        await axios.post('http://polls.local/api/questions/question', {
-            title,
-            author,
-            multipleAnswers,
-            addAnswers,
+        const res = await axios.post(
+            'http://polls.local/api/questions/question',
+            data
+        );
+        setData({
+            title: '',
+            author: '',
+            multipleAnswers: false,
+            addAnswers: false,
         });
 
-        console.log({ title, author, multipleAnswers, addAnswers });
-
-        setTitle('');
-        setAuthor('');
-        setMultipleAnswers(false);
-        setAddAnswers(false);
+        navigate(`/create/answers/${res.data._id}`, { state: res.data });
     };
 
     return (
@@ -31,30 +34,41 @@ const PollCreate = () => {
                 <div className="form-group">
                     <label>Poll title</label>
                     <input
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
+                        value={data.title}
+                        onChange={(e) =>
+                            setData({ ...data, title: e.target.value })
+                        }
                         className="form-control"
                     />
                     <label>Author</label>
                     <input
-                        value={author}
-                        onChange={(e) => setAuthor(e.target.value)}
+                        value={data.author}
+                        onChange={(e) =>
+                            setData({ ...data, author: e.target.value })
+                        }
                         className="form-control"
                     />
                     <label>Multiple Answers</label>
                     <input
                         type="checkbox"
-                        value={multipleAnswers}
-                        onChange={(e) => setMultipleAnswers(e.target.checked)}
-                        checked={multipleAnswers}
+                        value={data.multipleAnswers}
+                        onChange={(e) =>
+                            setData({
+                                ...data,
+                                multipleAnswers: e.target.checked,
+                            })
+                        }
+                        checked={data.multipleAnswers}
                     />
                     <br />
                     <label>Add Answers</label>
                     <input
                         type="checkbox"
-                        value={addAnswers}
-                        onChange={(e) => setAddAnswers(e.target.checked)}
-                        checked={addAnswers}
+                        value={data.addAnswers}
+                        onChange={(e) =>
+                            setData({ ...data, addAnswers: e.target.checked })
+                        }
+                        checked={data.addAnswers}
                     />
                 </div>
                 <button className="btn btn-primary">Create</button>
