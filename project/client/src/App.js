e01f98122f5b2e99d8fd0c78fc1ebfa426af27c0
Jan Pipan <janpipan@gmail.com>
2023-04-03 23:40:06 +0200

    added AddAnswers component

--- project/client/src/App.js
+++ project/client/src/App.js
@@ -4,6 +4,7 @@ import PollCreate from './pages/PollCreate';
 import Nav from './pages/Nav';
 import Home from './pages/Home';
 import Vote from './pages/Vote';
+import AddAnswers from './pages/AddAnswers';
 
 function App() {
     return (
@@ -11,7 +12,11 @@ function App() {
             <Routes>
                 <Route path="/" element={<Nav />}>
                     <Route index element={<Home />} />
-                    <Route path="create" element={<PollCreate />} />
+                    <Route path="create">
+                        <Route index element={<PollCreate />} />
+                        <Route path="answers/:id" element={<AddAnswers />} />
+                    </Route>
+
                     <Route path="vote/:id" element={<Vote />} />
                 </Route>
             </Routes>
---
+++ project/client/src/pages/AddAnswers.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import DisplayPoll from './DisplayPoll';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const AddAnswers = () => {
+    const navigate = useNavigate();
+    const { state } = useLocation();
+    const [answers, setAnswer] = useState(3);
+    const [data, setData] = useState(
+        Array.from(Array(answers)).map((x, index) => {
+            return {
+                answer: '',
+            };
+        })
+    );
+
+    console.log(state);
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        data.forEach(async (answer, index) => {
+            await axios.post(
+                `http://polls.local/api/answers/answer/${state._id}`,
+                answer
+            );
+            updateData(index, '');
+        });
+
+        navigate('/');
+    };
+
+    const onButtonClick = (event) => {
+        setAnswer(answers + 1);
+        setData((data) => [...data, { answer: '' }]);
+    };
+
+    const updateData = (i, value) => {
+        const newState = data.map((answer, index) => {
+            if (index === i) {
+                return { answer: value };
+            }
+            return answer;
+        });
+        setData(newState);
+    };
+
+    return (
+        <div className="container">
+            <DisplayPoll poll={state} />
+            <form onSubmit={onSubmit}>
+                <ul className="list-group">
+                    {Array.from(Array(answers)).map((x, index) => {
+                        return (
+                            <li className="list-group-item" key={index}>
+                                <div className="form-group">
+                                    <label>Answer {index + 1}</label>
+                                    <input
+                                        value={data[index].answer}
+                                        onChange={(e) =>
+                                            updateData(index, e.target.value)
+                                        }
+                                        className="form-control"
+                                        type="text"
+                                    />
+                                </div>
+                            </li>
+                        );
+                    })}
+                </ul>
+                <button className="btn btn-primary mt-2 mb-2">Submit</button>
+            </form>
+            <button className="btn btn-secondary" onClick={onButtonClick}>
+                Add
+            </button>
+        </div>
+    );
+};
+
+export default AddAnswers;
